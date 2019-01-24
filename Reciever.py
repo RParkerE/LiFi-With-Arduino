b88fc40bc69ebbcf42c57fdbcf45b2aacd6e2f89
Robert Parker Ellwanger <6766714+RParkerE@users.noreply.github.com>
2019-01-24 12:42:24 -0500

    Change Custom CRC To zlib.crc32
    Change our custom CRC function to use zlib.crc32 instead

--- Reciever.py
+++ Reciever.py
@@ -7,92 +7,25 @@ import sys
 import numpy
 import struct
 from array import array
+import zlib
+
 Tk().withdraw()
-
-key = "1001"
-
 
 file_name = askopenfilename()
 file = open(file_name, 'rb')
 file_obj = file.read()
-packet_num = int.from_bytes(file_obj[:3], 'big')
-file_obj = file_obj[3:]
+packet_num = int.from_bytes(file_obj[:4], 'big')
+checksum = int.from_bytes(file_obj[60:], 'big')
+file_obj = file_obj[4:60]
 
+def data_checker(data):
+    calc_crc = zlib.crc32(data)
+    if(checksum == calc_crc):
+        print("No Errors On Packet " + str(packet_num))
+    else:
+        print("Error Detectected On Packet " + str(packet_num))
 
-
-def xor(a, b): 
-   
-    # initialize result 
-    result = [] 
-   
-    # Traverse all bits, if bits are 
-    # same, then XOR is 0, else 1 
-    for i in range(1, len(b)): 
-        if a[i] == b[i]: 
-            result.append('0') 
-        else: 
-            result.append('1') 
-   
-    return ''.join(result) 
-   
-   
-# Performs Modulo-2 division 
-def mod2div(divident, divisor): 
-   
-    # Number of bits to be XORed at a time. 
-    pick = len(divisor) 
-   
-    # Slicing the divident to appropriate 
-    # length for particular step 
-    tmp = divident[0 : pick] 
-   
-    while pick < len(divident): 
-   
-        if tmp[0] == '1': 
-   
-            # replace the divident by the result 
-            # of XOR and pull 1 bit down 
-            tmp = xor(divisor, tmp) + divident[pick] 
-   
-        else:   # If leftmost bit is '0' 
-            # If the leftmost bit of the dividend (or the 
-            # part used in each step) is 0, the step cannot 
-            # use the regular divisor; we need to use an 
-            # all-0s divisor. 
-            tmp = xor('0'*pick, tmp) + divident[pick] 
-   
-        # increment pick to move further 
-        pick += 1
-   
-    # For the last n bits, we have to carry it out 
-    # normally as increased value of pick will cause 
-    # Index Out of Bounds. 
-    if tmp[0] == '1': 
-        tmp = xor(divisor, tmp) 
-    else: 
-        tmp = xor('0'*pick, tmp) 
-   
-    checkword = tmp 
-    return checkword 
-   
-# Function used at the receiver side to decode  
-# data received by sender 
-def decodeData(data, key): 
-   
-    l_key = len(key) 
-   
-    # Appends n-1 zeroes at end of data 
-    appended_data = data + '0'*(l_key-1) 
-    remainder = mod2div(appended_data, key) 
-   
-    return remainder 
-
-out = decodeData(file_obj.decode('utf-8'), key)
-
-if out == '000':
-    print('No Errors Detected')
-else:
-    print('Error Detected at ' + str(packet_num))
+data_checker(file_obj)
 
 
 
--- Sender.py
+++ Sender.py
@@ -8,122 +8,34 @@ import math
 import numpy
 import struct
 from array import array
+import zlib
 
 Tk().withdraw()
 
 count = 0
-key = "1001" 
 
 file_name = askopenfilename()
 file = open(file_name, 'rb')
 file_obj = file.read()
 file_size = len(file_obj)
-packet_num = math.ceil(file_size/60)
-padding_size = 60 - (file_size % 60)
+packet_num = math.ceil(file_size/56)
+padding_size = 60 - (file_size % 56)
 padding = '0' * padding_size
 file_obj = file_obj + padding.encode('utf-8')
-file_data = list(zip(*[iter(file_obj)]*60))
-
-#parity check function for any byte
-
-'''
-def parity_brute_force(x):
-    bit = 0
-    num_bits = 0
-    while x:
-        bitmask = 1 << bit
-        bit += 1
-        if x & bytes(bitmask):
-            num_bits += 1
-        x &= ~bytes(bitmask)
-    return num_bits % 2
-'''
+file_data = list(zip(*[iter(file_obj)]*56))
 
 #packet Creater adding 3 bytes index to 48 bytes of data
 def packet_creater(counter):
     data = file_data[counter]
-    dataByte = bytes(data)
+    data_byte = bytes(data)
     counter += 1
-    index = counter.to_bytes(3, 'big')
-    strData = str(dataByte.decode('utf-8'))
-    codedData = encodeData(strData,key).encode('utf-8')
-    packet = index + codedData
+    index = counter.to_bytes(4, 'big')
+    data_crc = zlib.crc32(data_byte) & 0xffffffff
+    data_crc = data_crc.to_bytes(4, 'big')
+    packet = index + data_byte + data_crc
+    print(packet)
     return packet
     
-   
-#CRC for error detection
-def xor(a, b): 
-   
-    # initialize result 
-    result = [] 
-   
-    # Traverse all bits, if bits are 
-    # same, then XOR is 0, else 1 
-    for i in range(1, len(b)): 
-        if a[i] == b[i]: 
-            result.append('0') 
-        else: 
-            result.append('1') 
-   
-    return ''.join(result) 
-   
-   
-# Performs Modulo-2 division 
-def mod2div(divident, divisor): 
-   
-    # Number of bits to be XORed at a time. 
-    pick = len(divisor) 
-   
-    # Slicing the divident to appropriate 
-    # length for particular step 
-    tmp = divident[0 : pick] 
-   
-    while pick < len(divident): 
-   
-        if tmp[0] == '1': 
-   
-            # replace the divident by the result 
-            # of XOR and pull 1 bit down 
-            tmp = xor(divisor, tmp) + divident[pick] 
-   
-        else:   # If leftmost bit is '0' 
-  
-            # If the leftmost bit of the dividend (or the 
-            # part used in each step) is 0, the step cannot 
-            # use the regular divisor; we need to use an 
-            # all-0s divisor. 
-            tmp = xor('0'*pick, tmp) + divident[pick] 
-   
-        # increment pick to move further 
-        pick += 1
-   
-    # For the last n bits, we have to carry it out 
-    # normally as increased value of pick will cause 
-    # Index Out of Bounds. 
-    if tmp[0] == '1': 
-        tmp = xor(divisor, tmp) 
-    else: 
-        tmp = xor('0'*pick, tmp) 
-   
-    checkword = tmp 
-    return checkword 
-   
-# Function used at the sender side to encode 
-# data by appending remainder of modular divison 
-# at the end of data. 
-def encodeData(data, key): 
-   
-    l_key = len(key) 
-   
-    # Appends n-1 zeroes at end of data 
-    appended_data = data + '0'*(l_key-1) 
-    remainder = mod2div(appended_data, key) 
-   
-    # Append remainder in the original data 
-    codeword = data + remainder 
-    return codeword     
-
-
 i = 0
 allData = b''
 while(i<packet_num):
