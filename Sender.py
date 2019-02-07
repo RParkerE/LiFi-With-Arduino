f17214ed1fbe8ecc9f2538703cec4982c468d302
Raheef Jalmiran <rgjalmir@ncsu.edu>
2019-02-07 11:49:54 -0500

    Add Gui, Tested Sender

--- Sender.py
+++ Sender.py
@@ -2,7 +2,7 @@ from tkinter import Tk
 from tkinter.filedialog import askopenfilename, asksaveasfilename
 
 # Other Imports
-import serial
+#import serial
 import sys
 import math
 import numpy
@@ -10,45 +10,49 @@ import struct
 from array import array
 import zlib
 
-Tk().withdraw()
+def sender_driver(file_name):
 
-count = 0
+    count = 0
 
-file_name = askopenfilename()
-file = open(file_name, 'rb')
-file_obj = file.read()
-file_size = len(file_obj)
-packet_num = math.ceil(file_size/56)
-padding_size = 60 - (file_size % 56)
-padding = '0' * padding_size
-file_obj = file_obj + padding.encode('utf-8')
-file_data = list(zip(*[iter(file_obj)]*56))
+    file = open(file_name, 'rb')
+    file_obj = file.read()
+    file_size = len(file_obj)
+    packet_num = math.ceil(file_size/56)
+    padding_size = 60 - (file_size % 56)
+    padding = '0' * padding_size
+    file_obj = file_obj + padding.encode('utf-8')
+    file_data = list(zip(*[iter(file_obj)]*56))
 
-#packet Creater adding 3 bytes index to 48 bytes of data
-def packet_creater(counter):
-    data = file_data[counter]
-    data_byte = bytes(data)
-    counter += 1
-    index = counter.to_bytes(4, 'big')
-    data_crc = zlib.crc32(data_byte) & 0xffffffff
-    data_crc = data_crc.to_bytes(4, 'big')
-    packet = index + data_byte + data_crc
-    print(packet)
-    return packet
+    #packet Creater adding 3 bytes index to 48 bytes of data
+    def packet_creater(counter):
+        data = file_data[counter]
+        data_byte = bytes(data)
+        counter += 1
+        index = counter.to_bytes(4, 'big')
+        data_crc = zlib.crc32(data_byte) & 0xffffffff
+        data_crc = data_crc.to_bytes(4, 'big')
+        packet = index + data_byte + data_crc
+        print(packet)
+        return packet
     
-i = 0
-allData = b''
-while(i<packet_num):
-    allData += packet_creater(count)
-    count += 1
-    i += 1
+    i = 0
+    allData = b''
+    while(i<packet_num):
+        allData += packet_creater(count)
+        count += 1
+        i += 1
 
-out_file_name = askopenfilename()
-out_file = open(out_file_name, 'wb')
-out_file.write(allData)
-file.close()
-out_file.close()
+    out_file_name = askopenfilename()
+    out_file = open(out_file_name, 'wb')
+    out_file.write(allData)
+    file.close()
+    out_file.close()
 
 
+def main(file_name):
+    sender_driver(file_name)
 
+"""if __name__ == "__main__":
+    main()"""
+    
     
