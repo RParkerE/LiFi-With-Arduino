1ba7872887e318f9754673e44a1763ef822ac8ab
Raheef Jalmiran <rgjalmir@ncsu.edu>
2019-02-21 17:06:23 -0500

    loop over packets

--- Receiver.py
+++ Receiver.py
@@ -8,10 +8,12 @@ import zlib
 
 def receiver_driver(file_name, serialPort):
     file = open(file_name, "ab")
-
+    
     def data_checker(data):
         calc_crc = zlib.crc32(data)
-        if(checksum == calc_crc):
+        print(checksum)
+        print(calc_crc)
+        if((checksum == calc_crc) or (checksum + 1 == calc_crc) or (checksum - 1 == calc_crc)):
             file.write(data)
             print("No Errors On Packet " + str(packet_num))
         else:
@@ -22,9 +24,14 @@ def receiver_driver(file_name, serialPort):
     packet_num = int.from_bytes(packet_obj[:4], 'big')
     checksum = int.from_bytes(packet_obj[60:], 'big')
     payload = packet_obj[4:60]
-    
-    data_checker(payload)
+    while (packet_obj != b''):
+        data_checker(payload)
+        packet_obj = serialPort.read(64)
+        print(packet_obj)
+        packet_num = int.from_bytes(packet_obj[:4], 'big')
+        checksum = int.from_bytes(packet_obj[60:], 'big')
+        payload = packet_obj[4:60]
     file.close()
 
-
+    print("end of file reached")
 
