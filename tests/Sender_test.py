808793f65c3331a333ac01497212e863cc75c5ed
Robert Parker Ellwanger <6766714+RParkerE@users.noreply.github.com>
2019-02-28 15:00:48 -0500

    Update Sender_test

--- tests/Sender_test.py
+++ tests/Sender_test.py
@@ -4,14 +4,12 @@ sys.path.append('../lib/')
 from Sender import Sender_Driver
 
 
-serial_port = serial.Serial(port="/dev/cu.usbmodem14201", baudrate=115200)
+serial_port = serial.Serial()
 
-sd = Sender_Driver('/Users/raheefjalmiran/Documents/GitHub/LiFi-Python/files/sender_test.txt', serial_port)
+sd = Sender_Driver("..\\files\\sender_test.txt", serial_port)
 
 meta = sd.meta_creator()
 print(meta)
 
 data_send = sd.packet_loop()
 print(data_send)
-
-print(sd.get_file_size())
