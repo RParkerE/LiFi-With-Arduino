0f7741d172480d06a3c5a072d6408d7362e5b1aa
Robert Parker Ellwanger <6766714+RParkerE@users.noreply.github.com>
2019-03-07 12:56:01 -0500

    Cross Platform File Path

--- lib/Receiver.py
+++ lib/Receiver.py
@@ -4,7 +4,7 @@ from pathlib import Path
 class Receiver_Driver:
 #def receiver_driver(file_name, serialPort):
 
-    def __init__(self, state_mach, file_name=Path("../files/out.txt"), serialPort=None):
+    def __init__(self, state_mach, file_name=Path(PureWindowsPath("..\\files\\out.txt")), serialPort=None):
         self.__packet_list = []
 
         self.__my_fsm = state_mach
